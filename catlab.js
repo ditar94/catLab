8c2d882d1333df9800eee37f66f44e98513b234f
Dionicy <dionicy@bu.edu>
2023-12-25 23:14:22 -0500

    this is the first version of my code

---
+++ catlab.js
@@ -0,0 +1,8 @@
+let cats = [
+    {name: "Bob Ross", likes: "painting"},
+    {name: "Gordon Ramsey", likes: "cooking"}
+];
+
+for (i = 0; i < cats.length; i++) {
+    console.log(cats[i].name + ' likes ' + cats[i].likes);
+}
